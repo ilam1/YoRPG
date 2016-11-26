e1075548071a57ca05b05419d5036bd36f29d10c
ilam1 <ilam1@stuy.edu>
2016-11-25 22:41:53 -0500

    Modified stats to be more reasonable

--- Boss.java
+++ Boss.java
@@ -1,8 +1,8 @@
 public class Boss extends Monster {
     public Boss() {
 	super();
-	_hitPts = 250;
-	_strength = 50;
+	_hitPts = 200;
+	_strength = 40;
 	_defense = 50;
 	_attack = Math.random() + 0.1;
     }
